adf4f46cd4d9edb1f95fd40fb9865628970e960c
Renato Saito <renato.saito@live.com>
2017-01-25 15:43:53 -0200

    Usando Octokit

---
+++ src/RandomHacksBh.Github.Api/Controllers/ReposController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Octokit;
+
+namespace RandomHacksBh.Github.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReposController : Controller
+    {
+        private readonly IGitHubClient _github;
+
+        public ReposController(IGitHubClient github)
+        {
+            _github = github;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var repos = await _github.Repository.GetAllForOrg("dotnet");
+                return StatusCode(200, repos);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ src/RandomHacksBh.Github.Api/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Octokit;
+
+namespace RandomHacksBh.Github.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddJsonOptions(opt =>
+                {
+                    opt.SerializerSettings.Formatting = Formatting.Indented;
+                });
+
+            services.AddScoped<IGitHubClient>(provider => new GitHubClient(new ProductHeaderValue("RandomHacksBh")));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
