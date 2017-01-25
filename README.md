f42bf2626ab84e64a94c6b74acb7388e104a932a
Corsino <guilhermecorsino@outlook.com>
2017-01-25 06:45:34 -0200

    Código de acesso a WebApi do GitHub

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GitHubApi
-WebApi  que consome a API do GitHub.
+Esse repositório contém uma API que irá intermediar alguns recursos da API do GitHub com a finalidade de possuirmos um WebApi em AspNet.Core para a interação da comunidade antes e depois dos Meetups realizados.
+
+* [Preparação próximo Meetup](https://www.meetup.com/pt-BR/Random-Hacks/events/237049580/): Xamarin Sem Preconceitos... - Uma app construida em Xamarin irá consumir esse WebApi para obter algumas informações do GitHub.
--- src/RH.Github.Api/Startup.cs
+++ src/RH.Github.Api/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -16,6 +11,7 @@ namespace RH.Github.Api
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,9 +24,13 @@ namespace RH.Github.Api
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.Run(async (context) =>
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
 			{
-				await context.Response.WriteAsync("Hello World!");
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
 			});
 		}
 	}
